97aeb937903dd6ff14bf5105423c8c148b7ed8d6
mestupinanm <111020866+mestupinanm@users.noreply.github.com>
2024-05-11 07:16:02 -0500

    ?

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('', include('measurements.urls')),
-    path('', include('variables.urls')),
+    path('', include('var.urls')),
 ]
 
